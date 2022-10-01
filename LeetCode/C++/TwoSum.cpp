7b7428fbfca54a49413916c60cc24852d18d70d4
Anant Chaudhary <78264061+anant0070@users.noreply.github.com>
2022-10-01 12:32:50 +0530

    Update TwoSum.cpp
    Beginner friendly with O(n^2) time complexity

--- LeetCode/C++/TwoSum.cpp
+++ LeetCode/C++/TwoSum.cpp
@@ -2,24 +2,17 @@
 using namespace std;
 
 
-vector<int> twoSum(vector<int> &numbers, int target)
+vector<int> twoSum(vector<int> &nums, int target)
 {
-    //Key is the number and value is its index in the vector.
-	unordered_map<int, int> hash;
-	vector<int> result;
-	for (int i = 0; i < numbers.size(); i++) {
-		int numberToFind = target - numbers[i];
-
-            //if numberToFind is found in map, return them
-		if (hash.find(numberToFind) != hash.end()) {
-                    //+1 because indices are NOT zero based
-			result.push_back(hash[numberToFind] + 1);
-			result.push_back(i + 1);			
-			return result;
-		}
-
-            //number was not found. Put it in the map.
-		hash[numbers[i]] = i;
-	}
-	return result;
+      vector<int> res;
+        
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if((nums[i]+nums[j])==target){
+                    res.push_back(i);
+                    res.push_back(j);
+                }
+            }
+        }
+        return res;
 }
