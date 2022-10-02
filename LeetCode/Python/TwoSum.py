244e16fba5b48126fa943311908441c25b90dfb4
aishikisikder <91593104+aishikisikder@users.noreply.github.com>
2022-10-02 12:27:49 +0530

    Update TwoSum.py

--- LeetCode/Python/TwoSum.py
+++ LeetCode/Python/TwoSum.py
@@ -1,13 +1,9 @@
 class Solution:
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    tmp = sorted(nums)
-    n, i, j = len(nums), 0, (n-1)
-    while True:
-        s = tmp[i]+tmp[j]
-        if s>target:
-            j-=1
-        elif s<target:
-            i+=1
-        else:
-            break
-    return [nums.index(tmp[i]),n-(nums[::-1].index(tmp[j]))-1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff = 0
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in nums:
+                idx = nums.index(diff)
+                if i!=idx:
+                    return [i,idx]
